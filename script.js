d0bff9fcb3a6271bcfe40bfe58b7474929a8c0f3
AMARTYA <amartya192006@gmail.com>
2026-01-06 10:02:34 +0530

    Update phone number format in orderNow function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function orderNow() {
   const message = encodeURIComponent(
     "Hello,\nI would like to place an order.\n\nItem:\nQuantity:\nPickup Time:"
   );
-  const phone = "8814884587";
+  const phone = "+91 XX148845XX";
   window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
 }
+
